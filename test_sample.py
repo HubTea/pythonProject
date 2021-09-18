c0f1c564973eabecd0105c5b234b4a1152423e05
HubTea <k3392255@naver.com>
2021-09-19 00:47:47 +0900

    pytest 연습용

---
+++ test_sample.py
@@ -0,0 +1,16 @@
+def inc(x):
+    return x + 1
+
+
+def test_answer():
+    assert 5 == 5
+
+
+def test_test():
+    assert print('hello')
+
+
+class TestCls:
+
+    def test_a(self):
+        assert 5 == 5
