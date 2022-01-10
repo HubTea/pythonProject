578ea73ab4f4236d0c9a3f24b015a56c708a2edc
HubTea <k3392255@naver.com>
2022-01-10 22:09:17 +0900

    Make test_mesh_plane_copy.py to use prepared_mesh.py

--- test_mesh_plane_copy.py
+++ test_mesh_plane_copy.py
@@ -1,45 +1,14 @@
 import mini3d
+import prepared_mesh
 
 
 def test_mesh_copy_plane_connection():
-    mesh = get_mesh_with_eight_planes()
-
+    mesh = prepared_mesh.mesh_with_eight_planes()
     plane_set = set(mesh.planes)
     mesh.copy_planes(plane_set)
     assert mesh.planes_count() == 8 + 8 + 8 * 2
 
-    mesh = get_mesh_with_eight_planes()
-
+    mesh = prepared_mesh.mesh_with_eight_planes()
     plane_set = set(mesh.planes)
     mesh.copy_planes(plane_set, False)
     assert mesh.planes_count() == 8 + 8
-
-
-def get_mesh_with_eight_planes():
-    coord_seq = [
-        [0, 0, 0],
-        [1, 0, 0],
-        [2, 0, 0],
-        [0, 1, 0],
-        [1, 1, 0],
-        [2, 1, 0],
-        [0, 2, 0],
-        [1, 2, 0],
-        [2, 2, 0]
-    ]
-
-    mesh = mini3d.Mesh()
-
-    vertex_seq = mesh.append_vertex(coord_seq)
-    mesh.make_plane(vertex_seq[0], vertex_seq[1], vertex_seq[3])
-    mesh.make_plane(vertex_seq[1], vertex_seq[4], vertex_seq[3])
-    mesh.make_plane(vertex_seq[1], vertex_seq[4], vertex_seq[5])
-    mesh.make_plane(vertex_seq[1], vertex_seq[2], vertex_seq[5])
-    mesh.make_plane(vertex_seq[5], vertex_seq[8], vertex_seq[7])
-    mesh.make_plane(vertex_seq[4], vertex_seq[5], vertex_seq[7])
-    mesh.make_plane(vertex_seq[3], vertex_seq[4], vertex_seq[7])
-    mesh.make_plane(vertex_seq[3], vertex_seq[7], vertex_seq[6])
-
-    # 위 코드에 오류 없는지 확인
-    assert mesh.planes_count() == 8 and mesh.is_valid()
-    return mesh
